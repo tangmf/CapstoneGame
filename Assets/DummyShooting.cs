72f40e1ac18b9abb5eb4c7a4d155ec4a67e5eb80
Tang Ming Feng <s10185023@connect.np.edu.sg>
2022-10-18 22:15:20 +0800

    Install tilemap

---
+++ Assets/DummyShooting.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DummyShooting : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform firePoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("d"))
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 currPos = transform.position;
+            Vector2 force = (mousePos - currPos).normalized;
+            if (force.x > 0)
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+            }
+            else if (force.x < 0)
+            {
+                transform.eulerAngles = new Vector3(0, 180, 0);
+            }
+            float rotation = Mathf.Atan2(force.y, force.x) * Mathf.Rad2Deg;
+            GameObject newBullet = (GameObject)Instantiate(bullet, firePoint.position, Quaternion.Euler(0f, 0f, rotation));
+        }
+    }
+}
