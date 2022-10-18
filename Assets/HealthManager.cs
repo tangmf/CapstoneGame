4ede5cc5bdac1a4527b890ead7774d8ef1a71339
Tang Ming Feng <s10185023@connect.np.edu.sg>
2022-10-18 22:05:34 +0800

    Simple HP manager

---
+++ Assets/HealthManager.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthManager : MonoBehaviour
+{
+    // References
+    public Slider healthBar;
+    // Variables
+    public float healthPoints;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Set bar to match entity hp
+        healthBar.maxValue = healthPoints;
+        // Set to max health
+        MaxHealth();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("s"))
+        {
+            Damage(20);
+        }
+        if (Input.GetKeyDown("w"))
+        {
+            Heal(20);
+        }
+    }
+
+    public void Damage(float dmg)
+    {
+        healthPoints -= dmg;
+        if(healthPoints <= 0)
+        {
+            healthPoints = 0;
+        }
+
+        healthBar.value = healthPoints;
+    }
+
+    public void Heal(float amt)
+    {
+        healthPoints += amt;
+        if (healthPoints >= healthBar.maxValue)
+        {
+            healthPoints = healthBar.maxValue;
+        }
+
+        healthBar.value = healthPoints;
+    }
+
+    public void MaxHealth()
+    {
+        healthBar.value = healthBar.maxValue;
+    }
+}
