2502bd5d0b9fe42c4c39655b0952e5f9c38407b7
Ho-Jun-An <Jun-An@Live.com>
2022-10-24 15:03:33 +0800

    Cleanup, added additional features to boss (Temporary)

--- Assets/BossBehaviour.cs
+++ Assets/BossBehaviour.cs
@@ -7,12 +7,15 @@ public class BossBehaviour : MonoBehaviour
     Animator animator;
     SpriteRenderer spriteRenderer;
     Rigidbody2D rb2d;
-    public bool playerDetect = false;
+
     public GameObject entity;
-    public Transform firePoint;
-    private float nextActionTime = 0.0f;
-    public float period = 1f;
     public GameObject bulletPrefab;
+    public Transform firePoint;
+
+    public bool playerDetect = false;
+
+    private float nextActionTime = 0.0f;
+    public float period = 2.5f;
 
     // Start is called before the first frame update
     void Start()
@@ -42,13 +45,24 @@ public class BossBehaviour : MonoBehaviour
     {
         if (Time.time > nextActionTime)
         {
+            createBullet(0);
+            createBullet(-20);
+            createBullet(-40);
+            createBullet(20);
+            createBullet(40);
 
-            GameObject newBullet = (GameObject)Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+            nextActionTime = Time.time + period;
+        }
+
+        void createBullet(float angle)
+        {
+            GameObject bullet = (GameObject)Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
 
             GameObject player = GameObject.FindWithTag("Player");
             Vector2 playerPos = player.transform.position;
             Vector2 currPos = transform.position;
             Vector2 force = (playerPos - currPos).normalized;
+
             if (force.x > 0)
             {
                 transform.eulerAngles = new Vector3(0, 0, 0);
@@ -57,15 +71,20 @@ public class BossBehaviour : MonoBehaviour
             {
                 transform.eulerAngles = new Vector3(0, 180, 0);
             }
-            newBullet.GetComponent<BulletBehaviour>().SetForce(force);
-            newBullet.GetComponent<BulletBehaviour>().ignoreTag = gameObject.tag;
-            newBullet.GetComponent<BulletBehaviour>().damageTag = "Player";
-            Destroy(newBullet, 2f);
 
+            bullet.GetComponent<BulletBehaviour>().SetForce(RotateVector(force, angle));
+            bullet.GetComponent<BulletBehaviour>().ignoreTag = gameObject.tag;
+            bullet.GetComponent<BulletBehaviour>().damageTag = "Player";
 
-            nextActionTime = Time.time + period;
+            Destroy(bullet, 2f);
         }
-        
 
+        Vector2 RotateVector(Vector2 v, float angle)
+        {
+            float radian = angle * Mathf.Deg2Rad;
+            float _x = v.x * Mathf.Cos(radian) - v.y * Mathf.Sin(radian);
+            float _y = v.x * Mathf.Sin(radian) + v.y * Mathf.Cos(radian);
+            return new Vector2(_x, _y);
+        }
     }
 }
