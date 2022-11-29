0b87a4b3b87a91d3585e8931e636be064b8e5337
Tang Ming Feng <s10185023@connect.np.edu.sg>
2022-11-29 11:54:05 +0800

    Working Dialogue System

---
+++ Assets/Ink/Dialogue01.ink
@@ -0,0 +1,12 @@
+﻿EXTERNAL Name(charName)
+EXTERNAL Portrait(charName)
+
+{Name("???")}
+{Portrait("02")}
+Line 1
+Line 2
+Line 3
+
+{Name("Person 1")}
+{Portrait("01")}
+I am person 1
---
+++ Assets/ScriptReader.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Ink.Runtime;
+using TMPro;
+
+public class ScriptReader : MonoBehaviour
+{
+    [SerializeField]
+    private TextAsset inkFile;
+    private Story story;
+
+    public TMP_Text dialogueBox;
+    public TMP_Text nameTag;
+
+    public Image charPortrait;
+
+    public GameObject nextBtn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadStory();
+
+        DisplayNextLine();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            DisplayNextLine();
+        }
+    }
+
+    void LoadStory()
+    {
+        story = new Story(inkFile.text);
+        story.BindExternalFunction("Name", (string charName)=> ChangeName(charName));
+        story.BindExternalFunction("Portrait", (string charName) => CharacterIcon(charName));
+
+    }
+
+    public void DisplayNextLine()
+    {
+        if (story.canContinue)
+        {
+            string text = story.Continue();
+            text = text?.Trim();
+            dialogueBox.text = text;
+            if (!story.canContinue)
+            {
+                Destroy(nextBtn);
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void ChangeName(string name)
+    {
+        string speakerName = name;
+
+        nameTag.text = speakerName;
+
+    }
+
+    public void CharacterIcon(string name)
+    {
+        var charIcon = Resources.Load<Sprite>("CharPortraits/"+name);
+        charPortrait.sprite = charIcon;
+    }
+}
