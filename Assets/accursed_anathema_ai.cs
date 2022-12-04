6721f2e0c03cd96d22d8f435134a855e4f1bb50c
Ho-Jun-An <Jun-An@Live.com>
2022-12-04 10:30:02 +0800

    Implemented simple boss movement

---
+++ Assets/accursed_anathema_ai.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class accursed_anathema_ai : MonoBehaviour
+{
+    Rigidbody2D body;
+    Animator animator;
+    public Transform player;
+
+    public bool isFlipped = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+
+        Physics2D.IgnoreLayerCollision(0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LookAtPlayer()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        Vector3 flipped = transform.localScale;
+        flipped.z *= -1f;
+
+        if (transform.position.x > player.position.x && isFlipped)
+        {
+            transform.localScale = flipped;
+            transform.Rotate(0f, 180f, 0f);
+            isFlipped = false;
+        }
+        else if (transform.position.x < player.position.x && !isFlipped)
+        {
+            transform.localScale = flipped;
+            transform.Rotate(0f, 180f, 0f);
+            isFlipped = true;
+        }
+    }
+}
---
+++ Assets/accursed_anathema_walking.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class accursed_anathema_walking : StateMachineBehaviour
+{
+    Rigidbody2D rigidbody;
+    Transform player;
+    accursed_anathema_ai boss;
+
+    public float moveSpeed = 0.8f;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        rigidbody = animator.GetComponent<Rigidbody2D>();
+        boss = animator.GetComponent<accursed_anathema_ai>();
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        boss.LookAtPlayer();
+
+        Vector2 target = new Vector2(player.position.x, rigidbody.position.y);
+        Vector2 newPos = Vector2.MoveTowards(rigidbody.position, target, moveSpeed * Time.fixedDeltaTime);
+        rigidbody.MovePosition(newPos);
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        
+    }
+}
