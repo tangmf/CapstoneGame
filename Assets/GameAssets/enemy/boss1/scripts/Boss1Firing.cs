983d3b18c7a634f5c3e3fc6e4813881bb60f9a5e
Ho-Jun-An <Jun-An@Live.com>
2023-01-02 17:02:09 +0800

    Added bullet sprites, edited arena

--- Assets/GameAssets/enemy/boss1/scripts/Boss1Firing.cs
+++ Assets/GameAssets/enemy/boss1/scripts/Boss1Firing.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class Boss1Firing : StateMachineBehaviour
 {
-    public float firingDuration;
-    public float firingLoop;
+    float firingDuration;
+    float firingLoop;
 
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         firingDuration = 3f;
-        firingLoop = 1f;
+        firingLoop = 0.5f;
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
@@ -29,21 +29,56 @@ public class Boss1Firing : StateMachineBehaviour
         }
 
         // Code for Firing Attack pattern
-        firingLoop -= Time.deltaTime;
         if (firingLoop > 0)
         {
             firingLoop -= Time.deltaTime;
         }
         else
         {
-            
-            firingLoop = 1f;
+            Fire();
+            firingLoop = 0.5f;
         }
     }
 
-    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    void Fire()
     {
-        
+        createBullet(0);
+        createBullet(-20);
+        createBullet(-40);
+        createBullet(20);
+        createBullet(40);
+
+        void createBullet(float angle)
+        {
+            GameObject bullet = (GameObject)Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+
+            GameObject player = GameObject.FindWithTag("Player");
+            Vector2 playerPos = player.transform.position;
+            Vector2 currPos = transform.position;
+            Vector2 force = (playerPos - currPos).normalized;
+
+            if (force.x > 0)
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+            }
+            else if (force.x < 0)
+            {
+                transform.eulerAngles = new Vector3(0, 180, 0);
+            }
+
+            bullet.GetComponent<BulletBehaviour>().SetForce(RotateVector(force, angle));
+            bullet.GetComponent<BulletBehaviour>().ignoreTag = gameObject.tag;
+            bullet.GetComponent<BulletBehaviour>().damageTag = "Player";
+
+            Destroy(bullet, 2f);
+        }
+
+        Vector2 RotateVector(Vector2 v, float angle)
+        {
+            float radian = angle * Mathf.Deg2Rad;
+            float _x = v.x * Mathf.Cos(radian) - v.y * Mathf.Sin(radian);
+            float _y = v.x * Mathf.Sin(radian) + v.y * Mathf.Cos(radian);
+            return new Vector2(_x, _y);
+        }
     }
 }
