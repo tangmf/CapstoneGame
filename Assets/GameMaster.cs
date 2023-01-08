d81d3d3a0c186f2a9b5c9fdbbec32eda91fd42d8
Tang Ming Feng <s10185023@connect.np.edu.sg>
2023-01-08 13:33:54 +0800

    Add spawn animation

--- Assets/GameMaster.cs
+++ Assets/GameMaster.cs
@@ -15,6 +15,9 @@ public class GameMaster : MonoBehaviour
     public GameObject winScreen;
     public GameObject popUp;
 
+    public GameObject boss;
+    public Transform bossSpawnPos;
+
     public GameObject gameStartScreen;
 
     void Start()
@@ -100,7 +103,8 @@ public class GameMaster : MonoBehaviour
     IEnumerator GameStart()
     {
         GameObject newGameStartScreen = Instantiate(gameStartScreen);
-        yield return new WaitForSeconds(1.0f);
+        yield return new WaitForSeconds(3.0f);
+        Instantiate(boss, bossSpawnPos.position, Quaternion.identity);
 
     }
 
