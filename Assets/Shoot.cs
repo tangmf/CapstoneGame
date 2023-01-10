5045a05fa00a29ff9a679ea45c64cfce9e81f3f5
Tang Ming Feng <s10185023@connect.np.edu.sg>
2023-01-10 22:58:38 +0800

    Add cardena as turret

--- Assets/Shoot.cs
+++ Assets/Shoot.cs
@@ -18,18 +18,25 @@ public class Shoot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        /*
         if (attackTime + cooldown < Time.time)
         {
-            // Create Bullet
-            GameObject newBullet = Instantiate(bullet, firePoint.position, Quaternion.Euler(0f, 0f, 0f));
-            newBullet.GetComponent<BulletBehaviour>().SetForce(new Vector2(1,0));
-            newBullet.GetComponent<BulletBehaviour>().ignoreTag = gameObject.tag;
-            newBullet.GetComponent<BulletBehaviour>().damageTag = "Player";
-
-            // Destroy after 2 seconds
-            Destroy(newBullet, 2f);
+            Shoot();
 
             attackTime = Time.time;
         }
+        */
+    }
+
+    public void ShootBullet()
+    {
+        // Create Bullet
+        GameObject newBullet = Instantiate(bullet, firePoint.position, Quaternion.Euler(0f, 0f, 0f));
+        newBullet.GetComponent<BulletBehaviour>().SetForce(new Vector2(1, 0));
+        newBullet.GetComponent<BulletBehaviour>().ignoreTag = gameObject.tag;
+        newBullet.GetComponent<BulletBehaviour>().damageTag = "Player";
+
+        // Destroy after 2 seconds
+        Destroy(newBullet, 2f);
     }
 }
