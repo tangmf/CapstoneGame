0441640edbdd5cbbe51654fbee36d4a2751817ae
Ho-Jun-An <Jun-An@Live.com>
2023-01-16 15:18:36 +0800

    Implemented boss2 movement

--- Assets/GameAssets/enemy/boss2/scripts/Boss2Flying.cs
+++ Assets/GameAssets/enemy/boss2/scripts/Boss2Flying.cs
@@ -7,10 +7,13 @@ public class Boss2Flying : StateMachineBehaviour
     Rigidbody2D rigidbody;
     Transform player;
     BossBehavior boss;
+    Transform bossTransform;
 
     public float moveSpeed;
 
     public float attackCooldown;
+
+    Vector2 flyPos;
 
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
@@ -18,17 +21,26 @@ public class Boss2Flying : StateMachineBehaviour
         player = GameObject.FindGameObjectWithTag("Player").transform;
         rigidbody = animator.transform.parent.GetComponent<Rigidbody2D>();
         boss = animator.transform.parent.GetComponent<BossBehavior>();
+        bossTransform = GameObject.FindGameObjectWithTag("Enemy").transform;
+
+        // Pick random fly position
+        flyPos = new Vector2(Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f));
+
+        Debug.Log(flyPos.ToString());
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        // Code for boss walking
+        // Code for boss flying
         boss.LookAtPlayer();
 
-        Vector2 target = new Vector2(player.position.x, rigidbody.position.y);
+        Vector2 bossPos = bossTransform.position;
+        bossTransform.position = Vector2.MoveTowards(bossPos, flyPos, moveSpeed * Time.deltaTime);
+
+        /*Vector2 target = new Vector2(player.position.x, rigidbody.position.y);
         Vector2 newPos = Vector2.MoveTowards(rigidbody.position, target, moveSpeed * Time.fixedDeltaTime);
-        rigidbody.MovePosition(newPos);
+        rigidbody.MovePosition(newPos);*/
 
         // Code for counting down to Firing Attack
         if (attackCooldown > 0)
