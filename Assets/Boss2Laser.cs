a37314d8ec1a062f8fdb87b44b7eb4351dfd9b44
Ho-Jun-An <Jun-An@Live.com>
2023-01-25 14:23:41 +0800

    Laser Telegraph fix for Boss2

---
+++ Assets/Boss2Laser.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss2Laser : MonoBehaviour
+{
+    Transform player;
+
+    public float laserLength;
+    public LineRenderer lineRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ShowLaser()
+    {
+        lineRenderer.enabled = true;
+    }
+
+    public void HideLaser()
+    {
+        lineRenderer.enabled = false;
+    }
+
+    public void LaserTelegraph(Vector3 playerPos, Vector3 currentPos)
+    {
+        playerPos[1] += 1.75f;
+        Vector3 offshootPos = (playerPos - currentPos) * laserLength;
+
+        Vector3[] laserPath = new Vector3[] { currentPos, offshootPos };
+        lineRenderer.SetPositions(laserPath);
+    }
+
+    public void LaserAttack(Vector3 playerPos, Vector3 currentPos)
+    {
+        playerPos[1] += 1.75f;
+        Vector3 offshootPos = (playerPos - currentPos) * laserLength;
+
+        Vector3[] laserPath = new Vector3[] { currentPos, offshootPos };
+        lineRenderer.SetPositions(laserPath);
+    }
+}
