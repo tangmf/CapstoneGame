6ff4ab05d9aaa46ccd0680c157e6e55903d76ee3
Tang Ming Feng <s10185023@connect.np.edu.sg>
2023-01-26 20:29:00 +0800

    Finish scoreboard functionality

--- Assets/ScoreBoardManager.cs
+++ Assets/ScoreBoardManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System.Linq;
 
 public class ScoreBoardManager : MonoBehaviour
 {
@@ -11,28 +12,42 @@ public class ScoreBoardManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void ShowScores(string level, ScoreDataList datas, string scene)
     {
         levelText.text = level;
         ClearScores();
+        List<ScoreData> newList = new List<ScoreData>();
 
         foreach (ScoreData data in datas.scoreDatas)
         {
             if (data.sceneName == scene)
             {
-                GameObject newObj = Instantiate(scoreContainer);
-                newObj.transform.parent = content;
-                newObj.GetComponent<ScoreContainer>().Set(data.grade, data.score, 1);
+                newList.Add(data);
             }
+        }
+
+        if (newList.Count > 0)
+        {
+            newList = newList.OrderBy(x => x.score).ToList();
+            newList.Reverse();
+        }
+        int index = 1;
+        foreach (ScoreData data in newList)
+        {
+            
+            GameObject newObj = Instantiate(scoreContainer);
+            newObj.transform.parent = content;
+            newObj.GetComponent<ScoreContainer>().Set(data.grade, data.score, index);
+            index++;
         }
     }
 
@@ -43,4 +58,5 @@ public class ScoreBoardManager : MonoBehaviour
             Destroy(x.gameObject);
         }
     }
+
 }
