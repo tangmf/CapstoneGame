0be75bf7a9ad997599907c6274f709713b00dd2a
Tang Ming Feng <s10185023@connect.np.edu.sg>
2023-01-29 00:19:39 +0800

    Add options scene and reset options

--- Assets/ResetPlayerPrefs.cs
+++ Assets/ResetPlayerPrefs.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class ResetPlayerPrefs : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        PlayerPrefs.DeleteAll();
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void Reset()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+
+    public void ResetScores()
+    {
+        try
+        {
+            string json = JsonUtility.ToJson("", true);
+            File.WriteAllText(Application.dataPath + "/ScoreDataFile.json", json);
+        }
+        catch
+        {
+
+        }
+
+
     }
 }
