b3c77826f9d455b77ea9516560f0c724cfd5a836
Ho-Jun-An <Jun-An@Live.com>
2023-01-31 22:52:07 +0800

    Created Boss4 arena, set up Boss4 skeleton and state machine outline

---
+++ Assets/GameAssets/enemy/boss4/scripts/Boss4Crawling.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss4Crawling : StateMachineBehaviour
+{
+    Rigidbody2D rigidbody;
+    Transform player;
+    BossBehavior boss;
+    Transform bossTransform;
+
+    public float moveSpeed;
+
+    public float attackCooldown;
+    float attackCountdown;
+
+    Vector2 flyPos;
+    public int minDistance = 15;
+    public Vector2 bottomLeftBounds = new Vector2(-18.0f,-3.0f);
+    public Vector2 topRightBounds = new Vector2(18.0f, 7.0f);
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        rigidbody = animator.transform.parent.GetComponent<Rigidbody2D>();
+        boss = animator.transform.GetComponent<BossBehavior>();
+        bossTransform = animator.transform;
+
+        attackCountdown = attackCooldown;
+
+        // Pick random fly position
+        flyPos = bossTransform.position;
+        while (true)
+        {
+            if (Vector2.Distance(bossTransform.position, flyPos) < minDistance)
+            {
+                flyPos = new Vector2(Random.Range(bottomLeftBounds.x, topRightBounds.x), Random.Range(bottomLeftBounds.y, topRightBounds.y));
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        // Code for boss flying
+        boss.LookAtPlayer();
+
+        Vector2 bossPos = bossTransform.position;
+        bossTransform.position = Vector2.MoveTowards(bossPos, flyPos, moveSpeed * Time.deltaTime);
+        if (bossPos == flyPos)
+        {
+            animator.SetBool("Boss_Attacking", true);
+            animator.SetTrigger("Boss_Fire");
+        }
+
+        /*Vector2 target = new Vector2(player.position.x, rigidbody.position.y);
+        Vector2 newPos = Vector2.MoveTowards(rigidbody.position, target, moveSpeed * Time.fixedDeltaTime);
+        rigidbody.MovePosition(newPos);*/
+        /*
+        // Code for counting down to Firing Attack
+        if (attackCountdown > 0)
+        {
+            attackCountdown -= Time.deltaTime;
+        }
+        else
+        {
+            animator.SetBool("Boss_Attacking", true);
+            animator.SetTrigger("Boss_Fire");
+            attackCountdown = attackCooldown;
+        }
+        */
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        
+    }
+}
---
+++ Assets/GameAssets/enemy/boss4/scripts/Boss4Firing.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss4Firing : StateMachineBehaviour
+{
+    Rigidbody2D rigidbody;
+    Transform player;
+    BossBehavior boss;
+
+    float exitTime = 0.0f;
+    float firingLoop = 0.7f;
+    public float stateDuration = 21.0f;
+    public bool shootBullet = false;
+
+    public float laserDelay = 1.5f;
+    public float laserDuration = 1f;
+    public bool shootLaser = false;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        rigidbody = animator.transform.GetComponent<Rigidbody2D>();
+        boss = animator.transform.GetComponent<BossBehavior>();
+        exitTime = Time.time + stateDuration;
+        if (shootLaser)
+        {
+            boss.StartShootLaser();
+        }
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        boss.LookAtPlayer();
+        // Code for counting down to Firing Attack
+        //if (firingDuration > 0)
+        if (Time.time < exitTime)
+        {
+            //firingDuration -= Time.deltaTime;
+        }
+        else
+        {
+            animator.SetBool("Boss_Attacking", false);
+            exitTime = 0.0f;
+        }
+
+        /*
+         
+        // Code for Firing Attack pattern
+        if (firingLoop > 0)
+        {
+            firingLoop -= Time.deltaTime;
+        }
+        else
+        {
+            if (shootBullet)
+            {
+                boss.Fire(5, 20);
+            }
+
+
+            firingLoop = 0.7f;
+        }
+        */
+
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+
+    }
+}
