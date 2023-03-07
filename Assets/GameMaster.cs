f28d8c36c02e8e8e08ca32a4b74ba33a44b9b656
Tang Ming Feng <s10185023@connect.np.edu.sg>
2023-03-07 15:41:40 +0800

    Revert change

--- Assets/GameMaster.cs
+++ Assets/GameMaster.cs
@@ -42,21 +42,7 @@ public class GameMaster : MonoBehaviour
 
     void Start()
     {
-        pm = GameObject.FindWithTag("PM").GetComponent<ProfileMaster>();
-        if (pm != null)
-        {
-            Debug.Log("PM OK");
-        }
-        else
-        {
-            Debug.Log("PM ERROR");
-        }
-        if (GameObject.FindGameObjectWithTag("WorldSpawn") != null)
-        {
-            transform.position = new Vector3(0, 0, 0);
-            Respawn();
-            StartCoroutine(GameStart());
-        }
+        
     }
 
     // Update is called once per frame
@@ -99,7 +85,21 @@ public class GameMaster : MonoBehaviour
             Destroy(gameObject);
         }
         */
-        
+        pm = GameObject.FindWithTag("PM").GetComponent<ProfileMaster>();
+        if (pm != null)
+        {
+            Debug.Log("PM OK");
+        }
+        else
+        {
+            Debug.Log("PM ERROR");
+        }
+        if (GameObject.FindGameObjectWithTag("WorldSpawn") != null)
+        {
+            transform.position = new Vector3(0, 0, 0);
+            Respawn();
+            StartCoroutine(GameStart());
+        }
 
     }
 
