a769d55bd74aecca790bb8da3d6a23bf976180c2
Woo Yan Seun <wooyanseun@gmail.com>
2023-03-18 22:51:05 +0800

    Interactable prefab added
    change sprite can alr

---
+++ Assets/Interactables/Interactable.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public Collider2D interactableAreaCollider;
+    public GameObject interactButton;
+    public string playerTag = "Player";
+    public int totalAllowedTimes = -1;
+    public int timesInteracted = 0;
+    public Vector3 interactButtonOffset = new Vector3(0, 1, 0);
+
+    private GameObject dialogueObject;
+    private bool isInArea;
+
+    void Update()
+    {
+        // Detect when the E arrow key is pressed down
+        if (isInArea && Input.GetKeyDown(KeyCode.E) && (totalAllowedTimes == -1 || (totalAllowedTimes != -1 && timesInteracted < totalAllowedTimes)))
+        {
+            Interact();
+            timesInteracted++;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag(playerTag))
+        {
+            isInArea = true;
+            // Show dialog box
+            if (interactButton)
+            {
+                dialogueObject = Instantiate(interactButton, this.transform.position + interactButtonOffset, this.transform.rotation);
+            }
+
+            // if (collision.gameObject.GetComponent<HealthManager>())
+            // {
+            //     if (!collision.gameObject.GetComponent<HealthManager>().dead && collision.gameObject.GetComponent<HealthManager>().healthPoints < collision.gameObject.GetComponent<HealthManager>().healthBar.maxValue)
+            //     {
+            //         if(effect != null)
+            //         {
+            //            Instantiate(effect, transform.position, transform.rotation);
+            //         }
+                    
+            //         if (hitSfx != null)
+            //         {
+
+            //             AudioSource.PlayClipAtPoint(hitSfx, transform.position);
+
+            //         }
+            //         collision.gameObject.GetComponent<HealthManager>().Heal(amt);
+            //         Destroy(gameObject);
+            //     }
+            // }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag(playerTag))
+        {
+            isInArea = false;
+            if (dialogueObject)
+            {
+                Destroy(dialogueObject);
+            }
+        }
+    }
+
+    public virtual void Interact()
+    {
+        // Overwrite
+        Debug.Log("Interacting with " + transform.name);
+    }
+}
---
+++ Assets/Interactables/InteractableWords.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class InteractableWords : Interactable
+{
+    public string textToDisplay;
+    public int fontSize = 30;
+    public GameObject textPrefab;
+    public float timeToShow = 1.0f;
+
+    private GameObject tempTextBox;
+
+    public override void Interact()
+    {
+        if (!tempTextBox)
+        {
+            tempTextBox = Instantiate(textPrefab, this.transform.position, this.transform.rotation);
+
+            // Shift to void Start?
+            GameObject textObject = tempTextBox.transform.GetChild(0).gameObject;
+            textObject.GetComponent<TMP_Text>().text = textToDisplay;
+            textObject.GetComponent<TMP_Text>().fontSize = fontSize;
+            
+            Destroy(tempTextBox, timeToShow);
+            tempTextBox = null;
+        }
+    }
+}
