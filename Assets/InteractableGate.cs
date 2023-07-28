fd5d59862f2a8d074b716f414faa37b1199b2975
tangmf <tangmf.sg@gmail.com>
2023-07-28 17:09:33 +0800

    Add transition animation for doors

--- Assets/InteractableGate.cs
+++ Assets/InteractableGate.cs
@@ -7,11 +7,24 @@ using TMPro;
 public class InteractableGate : Interactable
 {
     public Transform point;
-
+    public GameObject overlay;
+    public float overlayTime = 0.5f;
+    
     public override void Interact(GameObject go)
     {
         base.Interact(go);
+
+        // only trigger animation if player
+        if(go.CompareTag("Player")){
+            StartOverlay();
+        }
+        
         go.transform.position = point.position;
 
+    }
+
+    public void StartOverlay(){
+        GameObject newObj = Instantiate(overlay, new Vector3(0,0,0), Quaternion.identity);
+        newObj.transform.parent = this.transform;
     }
 }
